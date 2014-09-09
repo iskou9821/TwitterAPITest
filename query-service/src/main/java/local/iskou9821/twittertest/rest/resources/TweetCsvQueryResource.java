c4a61dc9b79f437dcc7d0cc525c3d231fd3feb3a
iskou9821 <kou.ishikawa@gmail.com>
2014-09-10 01:59:02 +0900

    いらないのけしたよ

--- query-service/src/main/java/local/iskou9821/twittertest/rest/resources/TweetCsvQueryResource.java
+++ query-service/src/main/java/local/iskou9821/twittertest/rest/resources/TweetCsvQueryResource.java
@@ -4,14 +4,11 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import local.iskou9821.twittertest.logic.TwitterQueryLogic;
 import local.iskou9821.twittertest.rest.model.TwitterCsvQueryConfigModel;
 import local.iskou9821.twittertest.util.TwitterFactoryUtil;
-
-import com.sun.jersey.core.util.Base64;
 
 @Path("/tweetCsvQuery")
 public class TweetCsvQueryResource {
@@ -23,13 +20,4 @@ public class TweetCsvQueryResource {
 		return (new TwitterQueryLogic()).queryCsv(model, TwitterFactoryUtil.getInstance());
 	}
 	
-	private String decodeBase64(String src) {
-		String s;
-		if (Base64.isBase64(src)) {
-			s = new String(Base64.decode(src));
-		} else {
-			s = src;
-		}
-		return s;
-	}
 }
