093f65b65700c939e9374e12ce287034b317ec46
iskou9821 <kou.ishikawa@gmail.com>
2014-09-10 01:57:50 +0900

    機能実装完了っぽい。

--- query-service/src/main/java/local/iskou9821/twittertest/rest/resources/TweetCsvQueryResource.java
+++ query-service/src/main/java/local/iskou9821/twittertest/rest/resources/TweetCsvQueryResource.java
@@ -1,17 +1,35 @@
 package local.iskou9821.twittertest.rest.resources;
 
-import javax.ws.rs.GET;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+
+import local.iskou9821.twittertest.logic.TwitterQueryLogic;
+import local.iskou9821.twittertest.rest.model.TwitterCsvQueryConfigModel;
+import local.iskou9821.twittertest.util.TwitterFactoryUtil;
+
+import com.sun.jersey.core.util.Base64;
 
 @Path("/tweetCsvQuery")
 public class TweetCsvQueryResource {
 
-	@GET
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.TEXT_PLAIN)
-	public String getCsv() {
-		return "text";
+	public String getCsv(TwitterCsvQueryConfigModel model) {
+		return (new TwitterQueryLogic()).queryCsv(model, TwitterFactoryUtil.getInstance());
 	}
 	
+	private String decodeBase64(String src) {
+		String s;
+		if (Base64.isBase64(src)) {
+			s = new String(Base64.decode(src));
+		} else {
+			s = src;
+		}
+		return s;
+	}
 }
--- query-service/src/test/java/local/iskou9821/twittertest/TwitterQueryTest.java
+++ query-service/src/test/java/local/iskou9821/twittertest/TwitterQueryTest.java
@@ -2,6 +2,7 @@ package local.iskou9821.twittertest;
 
 import junit.framework.TestCase;
 import local.iskou9821.twittertest.logic.TwitterQueryLogic;
+import local.iskou9821.twittertest.rest.model.TwitterCsvQueryConfigModel;
 import local.iskou9821.twittertest.rest.model.TwitterQueryConfigModel;
 import local.iskou9821.twittertest.rest.model.TwitterQueryResultDetailModel;
 import local.iskou9821.twittertest.rest.model.TwitterQueryResultModel;
@@ -9,6 +10,18 @@ import local.iskou9821.twittertest.util.TwitterFactoryUtil;
 import twitter4j.Twitter;
 
 public class TwitterQueryTest extends TestCase {
+	public void testCsvQuery() throws Exception {
+		Twitter tw = TwitterFactoryUtil.getInstance();
+		
+		TwitterCsvQueryConfigModel m = new TwitterCsvQueryConfigModel();
+		m.setCount(100);
+		m.setQueryText("#njslyr");
+		m.setHeaderText("hoge,hoge,hoge");
+		m.setTweetParseTemplate("${item.id}");//,${item.createdAt?datetime},${item.user.name},\"${item.text}\"");
+		
+		System.out.println((new TwitterQueryLogic()).queryCsv(m, tw));
+	}
+	
 	public void testQuery() throws Exception {
 		Twitter tw = TwitterFactoryUtil.getInstance();
 		
